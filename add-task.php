7da109dc792fe3a16584522394fa69182d4728c9
Tomasz Tabis <tomaszt@hotmail.nl>
2019-05-01 23:24:34 +0200

    Added connection
    Added connection class and database connection config

--- add-task.php
+++ add-task.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tomasz
- * Date: 1-5-2019
- * Time: 23:07
- */
---
+++ app/Connection.php
@@ -0,0 +1,24 @@
+<?php
+namespace Database;
+include __DIR__ . '/../config/Database.php';
+
+class Connection
+{
+
+    static public function createConnection(){
+        $db = new Database();
+        $server_address = $db->server_address;
+        $database_name = $db->database_name;
+        $username = $db->username;
+        $password = $db->password;
+
+        try{
+            $conn = new \PDO("mysql:host=" . $server_address . ";dbname=" . $database_name , $username, $password);
+            $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+//            $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION); //(To error's set it on)
+            return $conn;
+        }catch(\PDOException $e){
+            return 'Connection failed: ' . $e->getMessage();
+        }
+    }
+}
